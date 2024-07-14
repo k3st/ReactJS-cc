6f9d224dbb7e7a7778f00d8d9dfe5c2d151a74d1
k3st <kiddoturtle04@gmail.com>
2024-07-14 20:59:04 +0800

    added calculator buttons

--- scripts/landingpage.js
+++ scripts/landingpage.js
@@ -1,2 +1,35 @@
-const hello = <h1>Hello React</h1>;
-ReactDOM.render(hello, document.getElementById("root"));
+// const hello = <h1>Hello React</h1>;
+// ReactDOM.render(hello, document.getElementById("root"));
+
+function Calculator(){
+    return(
+
+        <div className="calculator">
+            <CalcButton value="7"/>
+            <CalcButton value="8"/>
+            <CalcButton value="9"/>
+            <CalcButton className="operator" value="/"/>
+
+            <CalcButton value="4"/>
+            <CalcButton value="5"/>
+            <CalcButton value="6"/>
+            <CalcButton className="operator" value="*"/>
+
+            <CalcButton value="1"/>
+            <CalcButton value="2"/>
+            <CalcButton value="3"/>
+            <CalcButton className="operator" value="-"/>
+
+            <CalcButton value="C"/>
+            <CalcButton value="0"/>
+            <CalcButton value="="/>
+            <CalcButton className="operator" value="+"/>
+        </div>
+    )
+}
+function CalcButton(property) {
+    return <button className={property.className}> {property.value}</button>
+
+}
+
+ReactDOM.render(<div classname="app-container"><Calculator/></div>,document.getElementById("root"));
