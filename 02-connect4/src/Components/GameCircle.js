d08112ca800fefcbd794d8c925e4c7c6ef546745
k3st <kiddoturtle04@gmail.com>
2024-07-16 00:15:19 +0800

    added css

--- 02-connect4/src/Components/GameCircle.js
+++ 02-connect4/src/Components/GameCircle.js
@@ -4,9 +4,19 @@ const onClick = (id, value) => {
   alert("click" + id);
 };
 
-const GameCircle = ({ id, value, children }) => {
-  console.log(id);
-  return <div onClick={(ev) => onClick(ev, id, value)}>{children}</div>;
+const GameCircle = ({ id, color, children }) => {
+  const style = {
+    backgroundColor: color,
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: 100,
+  };
+  return (
+    <div style={style} onClick={(ev) => onClick(ev, id)}>
+      {children}
+    </div>
+  );
 };
 
 export default GameCircle;
--- 02-connect4/src/Components/Gameboard.js
+++ 02-connect4/src/Components/Gameboard.js
@@ -2,20 +2,38 @@ import React from "react";
 import GameCircle from "./GameCircle";
 
 const GameBoard = () => {
+  const style = {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    gridTemplateRows: "1fr 1fr 1fr 1fr",
+    padding: 20,
+    width: "500px",
+    position: "absolute",
+    left: "10%",
+  };
+
   return (
-    <div>
-      <GameCircle id={1} value={5}>
-        Red
-      </GameCircle>
-      <GameCircle id={2} value={6}>
-        Blue
-      </GameCircle>
-      <GameCircle id={3}>Red</GameCircle>
-      <GameCircle id={4}>Blue</GameCircle>
-      <GameCircle id={5}>Red</GameCircle>
-      <GameCircle id={6}>Blue</GameCircle>
-      <GameCircle id={7}>Red</GameCircle>
-      <GameCircle id={8}>Blue</GameCircle>
+    <div style={style}>
+      <GameCircle id={1} color="red"></GameCircle>
+      <GameCircle id={2} color="blue"></GameCircle>
+      <GameCircle id={3} color="red"></GameCircle>
+      <GameCircle id={4} color="blue"></GameCircle>
+      <GameCircle id={5} color="red"></GameCircle>
+      <GameCircle id={6} color="blue"></GameCircle>
+      <GameCircle id={7} color="red"></GameCircle>
+      <GameCircle id={8} color="blue"></GameCircle>
+
+      <GameCircle id={9} color="red"></GameCircle>
+      <GameCircle id={10} color="blue"></GameCircle>
+
+      <GameCircle id={11} color="red"></GameCircle>
+      <GameCircle id={12} color="blue"></GameCircle>
+
+      <GameCircle id={13} color="red"></GameCircle>
+      <GameCircle id={14} color="blue"></GameCircle>
+
+      <GameCircle id={15} color="red"></GameCircle>
+      <GameCircle id={16} color="blue"></GameCircle>
     </div>
   );
 };
